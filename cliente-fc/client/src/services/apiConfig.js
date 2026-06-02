1c48269c5681bad68bd6d2dc0ca36efb573bc20a
Juan Ocaña <juandy.099@gmail.com>
2026-06-02 10:31:07 -0500

    apiconfig cambio a localhost

--- cliente-fc/client/src/services/apiConfig.js
+++ cliente-fc/client/src/services/apiConfig.js
@@ -1,5 +1,5 @@
-export const BASE_API_URL = 'http://192.168.100.4:5000';
-//export const BASE_API_URL = 'http://localhost:5000';
+//export const BASE_API_URL = 'http://192.168.100.4:5000';
+export const BASE_API_URL = 'http://localhost:5000';
 
 export const API_URL = `${BASE_API_URL}/api/fcc`;	
 export const API_IMAGE_URL = `${BASE_API_URL}`;
